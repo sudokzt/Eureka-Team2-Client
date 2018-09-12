0429fa803f3b50a9d8e68005b2768ffc42c010b8
Shawn Rivers <kaushou@gmail.com>
2018-09-12 12:16:10 +0900

    Add tutorial page and button linking to zero-crash-loading

--- src/containers/ZeroCrasherTutorial.js
+++ src/containers/ZeroCrasherTutorial.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ZeroCrasherTutorial extends Component {
   render() {
-    return <div>相性トーク：チュートリアル</div>;
+    return (
+      <div>
+        <div>
+          <h1>Image Here</h1>
+          <h2>相性トーク</h2>
+          <p>
+            相性トークは同じ趣味など共通の話題を持つ人とマッチング前に少し話せる機能です。
+          </p>
+        </div>
+        <Link to="/zero-crasher/loading">
+          <button className="text-btn single-line-btn">始める</button>
+        </Link>
+      </div>
+    );
   }
 }
 
