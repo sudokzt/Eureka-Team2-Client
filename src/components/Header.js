7ebee5946f98032e164e0b1a0b60e039a78b2008
Kazuto Mishima <wato9902@gmail.com>
2018-09-13 00:46:28 +0900

    タイマーを相性メッセージ画面上に表示　時間切れでモーダル表示

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./Header.css";
-import logoutBtn from "../images/logout-btn.svg";
+import './Header.css';
+import logoutBtn from '../images/logout-btn.svg';
 
 const Header = props => {
   let currentPage = null;
   if (props.currentPage) {
     currentPage = props.currentPage;
   }
+  let timeLimit = 0;
+  if (props.timeLimit) {
+    timeLimit = Number(props.timeLimit);
+  }
 
   let logout = null;
-  if (currentPage !== "Login") {
+  if (currentPage !== 'Login') {
     logout = (
       <Link to="/login" className="header__btn-holder">
         <img src={logoutBtn} alt="log out" className="header__btn" />
@@ -19,12 +23,25 @@ const Header = props => {
     );
   }
 
-  return (
-    <div className="header">
-      <h3>{currentPage}</h3>
-      {logout}
-    </div>
-  );
+  if (currentPage === 'ZeroCrasher') {
+    // timer
+    let min = ('00' + Math.floor(timeLimit / 60)).slice(-2);
+    let sec = ('00' + (timeLimit % 60)).slice(-2);
+    return (
+      <div className="timer__header">
+        <h3>
+          {min}:{sec}
+        </h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="header">
+        <h3>{currentPage}</h3>
+        {logout}
+      </div>
+    );
+  }
 };
 
 export default Header;
