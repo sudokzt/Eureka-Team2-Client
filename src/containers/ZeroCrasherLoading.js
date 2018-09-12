dfe5510d7902d1eeaa29317046788a7aebfcfcb7
Shawn Rivers <kaushou@gmail.com>
2018-09-12 13:42:33 +0900

    Add loading page and restart modal

--- src/containers/ZeroCrasherLoading.js
+++ src/containers/ZeroCrasherLoading.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
+import './ZeroCrasherLoading.css';
+import FailtoMatchModal from '../components/FailtoMatchModal';
+
 class ZeroCrasherLoading extends Component {
   render() {
-    return <div>相性トーク：ロード画面</div>;
+    return (
+      <div>
+        <div className="loading-indicator">
+          <h3>マッチング中...</h3>
+          <div className="loader" />
+        </div>
+        <FailtoMatchModal />
+      </div>
+    );
   }
 }
 
